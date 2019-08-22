b75e4cb522e5609eb02ccbea0761c78865f6de78
zachkohl <zach.kohl@gmail.com>
2019-08-22 12:14:34 -0700

    upload assets

--- index.js
+++ index.js
@@ -2,11 +2,6 @@ config = require('./config');
 
 config.app.get('/', function (req, res) {
    // res.send('hello world!')
-   res.render('home',{message:"hello world"})
+   res.render('home')
 });//end '/'
 
-
-config.app.get('/test', function (req, res) {
-
-    res.render('test',{message:"hello world"})
- });//end '/home'
