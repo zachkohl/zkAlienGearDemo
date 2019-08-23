6721d5d864fa768e64e30eb6b3deb064b8ff7e39
zachkohl <zach.kohl@gmail.com>
2019-08-22 23:18:39 -0700

    endforthenight

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 config = require('./config');
 
 config.app.get('/', function (req, res) {
-   // res.send('hello world!')
+
    res.render('home')
 });//end '/'
 
+config.app.get('/test', function (req, res) {
+
+   res.send('This would be another page of the site')
+});//end '/test'
